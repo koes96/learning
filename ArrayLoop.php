5a6c96239d8bfedd3f3cb4e86a2e744af603448a
dannykoes <dannykoes44@gmail.com>
2021-04-08 10:21:33 +0700

    tes nih

--- ArrayLoop.php
+++ ArrayLoop.php
@@ -1,6 +1,6 @@
 <?php
 $d = 0; //set nilai nol di variabel $d
-$aplikasi = array('gtAkademika', 'gtFinansialan', 'gtPerizinan', 'eCampuz', 'eOviz'); // set nilai array
+$aplikasi = array('gtAkademika', 'gtFinansialan', 'gtPerizinana', 'eCampuz', 'eOviz'); // set nilai array
 // buat perulangan untuk menampilkan array
 while ($d <= 4) {
 	echo $aplikasi[$d] . "</br>";
