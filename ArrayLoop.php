40a281a7f4fc2f9e1f38a4025971def73be9991b
dannykoes <dannykoes44@gmail.com>
2021-04-08 10:06:08 +0700

    tes satu

--- ArrayLoop.php
+++ ArrayLoop.php
@@ -1,9 +1,8 @@
 <?php
 $d = 0; //set nilai nol di variabel $d
-$aplikasi = array('gtAkademik','gtFinansial','gtPerizinan','eCampuz','eOviz'); // set nilai array
+$aplikasi = array('gtAkademika','gtFinansial','gtPerizinan','eCampuz','eOviz'); // set nilai array
 // buat perulangan untuk menampilkan array
 while ($d <= 4) {
 	echo $aplikasi[$d]."</br>";
 	$d++;
 }
-?>
